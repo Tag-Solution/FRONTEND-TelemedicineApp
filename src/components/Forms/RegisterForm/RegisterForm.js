bcb8dd5bec18ba690e5a41e711ba61d9f2ac3f0d
Tomas Padilla <padillatomasagustin@gmail.com>
2023-04-28 16:53:34 +0200

    auth - CORS

--- src/components/Forms/RegisterForm/RegisterForm.js
+++ src/components/Forms/RegisterForm/RegisterForm.js
@@ -1,82 +1,81 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 import Wrapper from "./RegisterForm.styles";
 
-const RegisterForm = () => {
-	const formik = useFormik({
-		initialValues: {
-			email: "",
-			password: "",
-			firstName: "",
-			lastName: "",
-			roleId: 1,
-		},
-		onSubmit: (values) => {
-			alert(JSON.stringify(values, null, 2));
-		},
-	});
+import { registerUser } from "../../../services/auth_service";
 
+const RegisterForm = () => {
 	return (
 		<Wrapper>
-			<form onSubmit={formik.handleSubmit} className="register-form">
-				<div className="input-block">
-					{/* Email */}
-					<div className="single-input">
-						<label htmlFor="email">Email</label>
-						<input
-							type="text"
-							name="email"
-							placeholder="E-mail"
-							id="email"
-							onChange={formik.handleChange}
-							value={formik.values.email}
-						/>
+			<Formik
+				initialValues={{
+					email: "",
+					password: "",
+					firstName: "",
+					lastName: "",
+					photo:
+						"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F08%2Fsimp_homersingle08_f_hires2-2000.jpg",
+					roleId: 1,
+				}}
+				validationSchema={Yup.object({
+					firstName: Yup.string()
+						.max(15, "Must be 15 characters or less")
+						.required("Required"),
+					lastName: Yup.string()
+						.max(20, "Must be 20 characters or less")
+						.required("Required"),
+					email: Yup.string()
+						.email("Invalid email address")
+						.required("Required"),
+				})}
+				onSubmit={(values) => {
+					registerUser(values)
+						.then((response) => {
+							console.log(response);
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				}}
+			>
+				<Form className="register-form">
+					<div className="input-block">
+						{/* Email */}
+						<div className="single-input">
+							<label htmlFor="email">Email</label>
+							<Field name="email" placeholder="E-mail" />
+							<ErrorMessage name="email" component="div" />
+						</div>
+
+						{/* Password */}
+						<div className="single-input">
+							<label htmlFor="password">Contraseña</label>
+							<Field name="password" />
+						</div>
 					</div>
-					{/* Password */}
-					<div className="single-input">
-						<label htmlFor="password">Contraseña</label>
-						<input
-							type="password"
-							name="password"
-							placeholder="Ingrese su contraseña"
-							id="password"
-							onChange={formik.handleChange}
-							value={formik.values.password}
-						/>
+
+					<div className="input-block">
+						{/* First_Name */}
+						<div className="single-input">
+							<label htmlFor="firstName">Nombre</label>
+							<Field name="firstName" placeholder="First Name" />
+							<ErrorMessage name="firstName" component="div" />
+						</div>
+
+						{/* Last_Name */}
+						<div className="single-input">
+							<label htmlFor="lastName">Apellido</label>
+							<Field name="lastName" placeholder="Ej: Fulanito" />
+							<ErrorMessage name="lastName" component="div" />
+						</div>
 					</div>
-				</div>
-				{/* === PERSONA === */}
-				<div className="input-block">
-					{/* First_Name */}
-					<div className="single-input">
-						<label htmlFor="firstName">Nombre</label>
-						<input
-							type="text"
-							name="firstName"
-							placeholder="Ej: Cosme"
-							id="firstName"
-							onChange={formik.handleChange}
-							value={formik.values.firstName}
-						/>
-					</div>
-					{/* Last_Name */}
-					<div className="single-input">
-						<label htmlFor="lastName">Apellido</label>
-						<input
-							type="text"
-							name="lastName"
-							placeholder="Ej: Fulanito"
-							id="lastName"
-							onChange={formik.handleChange}
-							value={formik.values.lastName}
-						/>
-					</div>
-				</div>
-				<button type="submit" className="btn-white">
-					Submit
-				</button>
-			</form>
+					<button type="submit" className="btn-white">
+						Submit
+					</button>
+				</Form>
+			</Formik>
 		</Wrapper>
 	);
 };
---
+++ src/services/auth_service.js
@@ -0,0 +1,10 @@
+import AxiosConfig from "./axios/config/axios.config";
+import { API_REGISTER, API_LOGIN } from "../utils/api_constants";
+
+export function registerUser(body) {
+	return AxiosConfig.post(API_REGISTER, body, {
+		validateStatus: function (status) {
+			return status < 500;
+		},
+	});
+}
