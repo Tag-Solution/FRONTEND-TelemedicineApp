e0d354db7a844b3ad8e198ce29bdb73497a1c45c
Tomas Padilla <padillatomasagustin@gmail.com>
2023-04-28 15:44:54 +0200

    auth - Register styles

--- src/components/Forms/RegisterForm/RegisterForm.js
+++ src/components/Forms/RegisterForm/RegisterForm.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { useFormik } from "formik";
 
 import Wrapper from "./RegisterForm.styles";
@@ -9,23 +8,17 @@ const RegisterForm = () => {
 		initialValues: {
 			email: "",
 			password: "",
-			first_name: "",
-			last_name: "",
-			age: 0,
-			dni: "",
-			country: "",
-			province: "",
-			city: "",
+			firstName: "",
+			lastName: "",
+			roleId: 1,
 		},
 		onSubmit: (values) => {
 			alert(JSON.stringify(values, null, 2));
 		},
 	});
 
-	let navigate = useNavigate();
-
 	return (
-		<Wrapper className="form-container">
+		<Wrapper>
 			<form onSubmit={formik.handleSubmit} className="register-form">
 				<div className="input-block">
 					{/* Email */}
@@ -44,7 +37,7 @@ const RegisterForm = () => {
 					<div className="single-input">
 						<label htmlFor="password">Contraseña</label>
 						<input
-							type="text"
+							type="password"
 							name="password"
 							placeholder="Ingrese su contraseña"
 							id="password"
@@ -60,90 +53,29 @@ const RegisterForm = () => {
 						<label htmlFor="firstName">Nombre</label>
 						<input
 							type="text"
-							name="first_name"
+							name="firstName"
 							placeholder="Ej: Cosme"
-							id="first_name"
+							id="firstName"
 							onChange={formik.handleChange}
-							value={formik.values.first_name}
+							value={formik.values.firstName}
 						/>
 					</div>
 					{/* Last_Name */}
 					<div className="single-input">
-						<label htmlFor="last_name">Apellido</label>
+						<label htmlFor="lastName">Apellido</label>
 						<input
 							type="text"
-							name="last_name"
+							name="lastName"
 							placeholder="Ej: Fulanito"
-							id="last_name"
+							id="lastName"
 							onChange={formik.handleChange}
-							value={formik.values.last_name}
-						/>
-					</div>
-					{/* DNI */}
-					<div className="single-input">
-						<label htmlFor="dni">DNI</label>
-						<input
-							type="text"
-							name="dni"
-							placeholder="Ej: 35000000"
-							id="dni"
-							onChange={formik.handleChange}
-							value={formik.values.dni}
-						/>
-					</div>
-					{/* Age */}
-					<div className="single-input">
-						<label htmlFor="dni">Edad</label>
-						<input
-							type="number"
-							name="age"
-							placeholder="Ej: 38"
-							id="age"
-							onChange={formik.handleChange}
-							value={formik.values.age}
+							value={formik.values.lastName}
 						/>
 					</div>
 				</div>
-				{/* === COORDONEES === */}
-				<div className="input-block">
-					{/* Country */}
-					<div className="single-input">
-						<label htmlFor="country">Pais</label>
-						<input
-							type="text"
-							name="country"
-							placeholder="Ej: Argentina"
-							id="country"
-							onChange={formik.handleChange}
-							value={formik.values.country}
-						/>
-					</div>
-					{/* Province */}
-					<div className="single-input">
-						<label htmlFor="province">Provincia</label>
-						<input
-							type="text"
-							name="province"
-							placeholder="Ej: Buenos Aires"
-							id="province"
-							onChange={formik.handleChange}
-							value={formik.values.province}
-						/>
-					</div>
-					{/* City */}
-					<div className="single-input">
-						<label htmlFor="city">Ciudad</label>
-						<input
-							type="text"
-							name="city"
-							placeholder="Ej: Olivos"
-							id="city"
-							onChange={formik.handleChange}
-							value={formik.values.city}
-						/>
-					</div>
-				</div>
-				<button type="submit">Submit</button>
+				<button type="submit" className="btn-white">
+					Submit
+				</button>
 			</form>
 		</Wrapper>
 	);
--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -43,6 +43,7 @@ const NavContainer = styled.nav`
 	justify-content: center;
 	background: var(--ColorWhite);
 	color: var(--ColorBlack);
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
 	.nav-center {
 		width: 90%;
 		margin: 0 auto;
