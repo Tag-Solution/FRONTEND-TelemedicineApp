adf09f37a33fc190919bac931be3e91dc7344a53
Tomas Padilla <padillatomasagustin@gmail.com>
2023-05-10 12:46:49 +0200

    TAF3 - Dashboard comenzamos

--- src/components/Navigation/Dashboard/DashboardNavigation.styles.js
+++ src/components/Navigation/Dashboard/DashboardNavigation.styles.js
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.aside``;
+export const Wrapper = styled.aside`
+	width: 100%;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	height: 3rem;
+	background: red;
+	display: grid;
+	place-items: center;
+`;
--- src/utils/routes_constants.js
+++ src/utils/routes_constants.js
@@ -31,6 +31,39 @@ export const links = [
 	},
 ];
 
+export const dashboard_links = [
+	{
+		id: 1,
+		text: "Home",
+		path: "/",
+		icon: null,
+	},
+	{
+		id: 2,
+		text: "About Us",
+		path: "/about",
+		icon: null,
+	},
+	{
+		id: 3,
+		text: "Contact",
+		path: "/contact",
+		icon: null,
+	},
+	{
+		id: 4,
+		text: "Register",
+		path: "/auth/register",
+		icon: null,
+	},
+	{
+		id: 5,
+		text: "Login",
+		path: "/auth/login",
+		icon: null,
+	},
+];
+
 export const routes = [
 	{
 		id: 1,
