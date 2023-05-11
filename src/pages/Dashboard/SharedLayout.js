0efd9e9e2373335eedcc199c51e540442ee86738
Tomas Padilla <padillatomasagustin@gmail.com>
2023-05-11 13:03:57 +0200

    deploy -height

--- src/pages/Dashboard/SharedLayout.js
+++ src/pages/Dashboard/SharedLayout.js
@@ -2,13 +2,22 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Wrapper } from "./SharedLayout.styles";
 
-import { DashboardNavigation } from "../../components";
+import { DashboardNavigation, useWindowDimensions } from "../../components";
 
 const SharedLayout = () => {
+	const { height } = useWindowDimensions();
+	console.log("====================================");
+	console.log(height);
+	console.log("====================================");
 	return (
 		<Wrapper>
 			<DashboardNavigation></DashboardNavigation>
-			<div className="outlet-container">
+			<div
+				className="outlet-container"
+				style={{
+					height: `calc(${height} - 4rem)`,
+				}}
+			>
 				<Outlet></Outlet>
 			</div>
 		</Wrapper>
--- src/pages/Dashboard/SharedLayout.styles.js
+++ src/pages/Dashboard/SharedLayout.styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Wrapper = styled.section`
 	.outlet-container {
-		height: calc(100vh - 4rem);
 		background: var(--ColorWhite-2);
 	}
 `;
